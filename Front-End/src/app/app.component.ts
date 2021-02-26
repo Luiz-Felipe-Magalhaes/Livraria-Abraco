80a428f29638238fd37c3d13635863d92a9ce13c
Luiz-Felipe-Magalhaes <luizfelipe.magalhaes@outlook.com>
2021-02-26 00:01:38 -0300

    Criação de componentes e implantação de rotas.

--- Front-End/src/app/app.component.ts
+++ Front-End/src/app/app.component.ts
@@ -1,10 +1,1 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'LivrariaAbraco';
-}
+<app-loja> <app-loja>
